ffc313dfa428b8e1fd30d526f07e60985da633f7
pva <tango2002@gmail.com>
2023-02-27 00:07:19 +0300

    fix: magic numbers

--- src/by/tms/lesson15/homework/task1/BlrPassport.java
+++ src/by/tms/lesson15/homework/task1/BlrPassport.java
@@ -5,15 +5,18 @@ import java.time.format.DateTimeFormatter;
 
 public class BlrPassport {
 
-    private String idNumber;
-    private String name;
-    private String surname;
-    private Gender gender;
-    private LocalDate birthDate;
-    private String passNumber;
-    private LocalDate beginDate;
-    private LocalDate endDate;
+    private final String idNumber;
+    private final String name;
+    private final String surname;
+    private final Gender gender;
+    private final LocalDate birthDate;
+    private final String passNumber;
+    private final LocalDate beginDate;
+    private final LocalDate endDate;
 
+
+    public static final int PASSPORT_ID = 14;
+    public static final int PASSPORT_NUMBER = 9;
 
     public BlrPassport(String idNumber, String name, String surname, Gender gender, LocalDate birthDate,
                        String passNumber, LocalDate beginDate, LocalDate endDate) {
@@ -35,14 +38,14 @@ public class BlrPassport {
 
 
     public boolean isExpared(LocalDate now) {
-        return LocalDate.now().isAfter(endDate);
+        return now.isAfter(endDate);
     }
 
     private boolean isValidPassport(String idNumber, String name, String surname,
                                     String passNumber, LocalDate beginDate, LocalDate endDate) {
 
         return isValidIdNumber(idNumber) && isValidNameAndSurname(name, surname) && isGoodPassNumber(passNumber)
-               && isGoodDataPassDuration(beginDate, endDate);
+                && isGoodDataPassDuration(beginDate, endDate);
 
     }
 
@@ -51,7 +54,7 @@ public class BlrPassport {
     }
 
     private boolean isGoodPassNumber(String passNumber) {
-        if (passNumber.length() != 9) {
+        if (passNumber.length() != PASSPORT_NUMBER) {
             throw new IllegalArgumentException("Passport number should contains 9 signs");
         }
         return passNumber.matches("(AB|BM|HB|KH|MP|MC|KB|PP|SP|DP)\\d{7}");
@@ -67,7 +70,7 @@ public class BlrPassport {
 
     private boolean isValidIdNumber(String idNumber) {
 
-        if (idNumber.length() != 14) {
+        if (idNumber.length() != PASSPORT_ID) {
             throw new IllegalArgumentException("Passport identification number should contains 14 signs");
         }
         return idNumber.matches("\\d{7}[ABCKEMH]\\d{3}(PB|BA|BI)\\d");
@@ -78,14 +81,14 @@ public class BlrPassport {
     public String toString() {
 
         return "Belarusian Passport: \n" +
-               "\t\t passport number = " + idNumber + "\n" +
-               "\t\t owners name = " + name + "\n" +
-               "\t\t owners surname = " + surname + "\n" +
-               "\t\t owners gender = " + gender + "\n" +
-               "\t\t owners birthDate = " + birthDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")) + "\n" +
-               "\t\t passport personal number = " + passNumber + "\n" +
-               "\t\t passport valid: from = " + beginDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")) +
-               " to " + endDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+                "\t\t passport number = " + idNumber + "\n" +
+                "\t\t owners name = " + name + "\n" +
+                "\t\t owners surname = " + surname + "\n" +
+                "\t\t owners gender = " + gender + "\n" +
+                "\t\t owners birthDate = " + birthDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")) + "\n" +
+                "\t\t passport personal number = " + passNumber + "\n" +
+                "\t\t passport valid: from = " + beginDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")) +
+                " to " + endDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
 }
